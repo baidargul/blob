7dd3134f38ed14a72bd845c3e7a91e8951c771b7
Baidar Gul <baidargul@outlook.com>
2024-10-06 12:17:42 +0500

    created main menu menus

--- components/structure/MainMenu.tsx
+++ components/structure/MainMenu.tsx
@@ -6,8 +6,11 @@ import {
   MenubarMenu,
   MenubarSeparator,
   MenubarShortcut,
+  MenubarSubContent,
+  MenubarSubTrigger,
   MenubarTrigger,
 } from "@/components/ui/menubar";
+import { MenubarSub } from "@radix-ui/react-menubar";
 
 type Props = {};
 
@@ -17,15 +20,100 @@ const MainMenu = (props: Props) => {
       <MenubarMenu>
         <MenubarTrigger>File</MenubarTrigger>
         <MenubarContent>
-          <MenubarItem>
-            New Tab <MenubarShortcut>⌘T</MenubarShortcut>
-          </MenubarItem>
-          <MenubarItem>New Window</MenubarItem>
+          <MenubarItem>Audit</MenubarItem>
+          <MenubarItem>Dashboard creator</MenubarItem>
           <MenubarSeparator />
-          <MenubarItem>Share</MenubarItem>
+          <MenubarItem>IMEI generator</MenubarItem>
           <MenubarSeparator />
-          <MenubarItem>Print</MenubarItem>
+          <MenubarItem>Reset</MenubarItem>
         </MenubarContent>
+      </MenubarMenu>
+      <MenubarMenu>
+        <MenubarTrigger>Edit</MenubarTrigger>
+        <MenubarContent>
+          <MenubarItem>Settings</MenubarItem>
+          <MenubarItem>Purge</MenubarItem>
+        </MenubarContent>
+      </MenubarMenu>
+      <MenubarMenu>
+        <MenubarTrigger>Products</MenubarTrigger>
+        <MenubarContent>
+          <MenubarItem>Product list</MenubarItem>
+          <MenubarItem>Inventory</MenubarItem>
+          <MenubarSub>
+            <MenubarSubTrigger>Find</MenubarSubTrigger>
+            <MenubarSubContent>
+              <MenubarItem>IMEI Search</MenubarItem>
+              <MenubarSeparator />
+              <MenubarItem>Find...</MenubarItem>
+            </MenubarSubContent>
+          </MenubarSub>
+          <MenubarSeparator />
+          <MenubarItem>Brand list</MenubarItem>
+          <MenubarSeparator />
+          <MenubarItem>Category list</MenubarItem>
+          <MenubarSeparator />
+          <MenubarItem>Type list</MenubarItem>
+        </MenubarContent>
+      </MenubarMenu>
+      <MenubarMenu>
+        <MenubarTrigger>Customers</MenubarTrigger>
+        <MenubarContent>
+          <MenubarItem>Customers list</MenubarItem>
+          <MenubarItem>Find...</MenubarItem>
+        </MenubarContent>
+      </MenubarMenu>
+      <MenubarMenu>
+        <MenubarTrigger>Vendors</MenubarTrigger>
+        <MenubarContent>
+          <MenubarItem>Vendor list</MenubarItem>
+          <MenubarItem>Find...</MenubarItem>
+        </MenubarContent>
+      </MenubarMenu>
+      <MenubarMenu>
+        <MenubarTrigger>Employees</MenubarTrigger>
+        <MenubarContent>
+          <MenubarItem>Employee list</MenubarItem>
+          <MenubarItem>Find...</MenubarItem>
+        </MenubarContent>
+      </MenubarMenu>
+      <MenubarMenu>
+        <MenubarTrigger>Sales</MenubarTrigger>
+        <MenubarContent>
+          <MenubarItem>Create quotation</MenubarItem>
+          <MenubarItem>Create Invoice</MenubarItem>
+          <MenubarSeparator />
+          <MenubarItem>Invoice list</MenubarItem>
+          <MenubarItem>Find...</MenubarItem>
+        </MenubarContent>
+      </MenubarMenu>
+      <MenubarMenu>
+        <MenubarTrigger>Purchases</MenubarTrigger>
+        <MenubarContent>
+          <MenubarItem>Create quotation</MenubarItem>
+          <MenubarItem>Create purchase order</MenubarItem>
+          <MenubarSeparator />
+          <MenubarItem>Purchase list</MenubarItem>
+          <MenubarItem>Find...</MenubarItem>
+        </MenubarContent>
+      </MenubarMenu>
+      <MenubarMenu>
+        <MenubarTrigger>Accounts</MenubarTrigger>
+        <MenubarContent>
+          <MenubarItem>Account list</MenubarItem>
+          <MenubarItem>Create an account</MenubarItem>
+          <MenubarItem>Purchase list</MenubarItem>
+          <MenubarItem>Find...</MenubarItem>
+        </MenubarContent>
+      </MenubarMenu>
+      <MenubarMenu>
+        <MenubarTrigger>Reports</MenubarTrigger>
+        {/* <MenubarContent>
+          <MenubarItem>Account list</MenubarItem>
+          <MenubarItem>Create an account</MenubarItem>
+          <MenubarItem>Purchase list</MenubarItem>
+          <MenubarItem>Find...</MenubarItem>
+        </MenubarContent> */}
       </MenubarMenu>
     </Menubar>
   );
