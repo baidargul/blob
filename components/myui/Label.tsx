5a23a256502f8c4176b90effb7438f971d273589
Baidar Gul <baidargul@outlook.com>
2024-10-19 18:16:07 +0500

    added product list to product quotation

--- components/myui/Label.tsx
+++ components/myui/Label.tsx
@@ -4,14 +4,15 @@ type Props = {
   label: string;
   size?: "text-xs" | "text-sm" | "text-md" | "text-lg";
   className?: string;
+  color?: string;
 };
 
 const Label = (props: Props) => {
   return (
     <div
-      className={`${
-        props.size ? props.size : "text-base"
-      } font-semibold text-interface-text/80 ${props.className}`}
+      className={`${props.size ? props.size : "text-base"} font-semibold  ${
+        props.className
+      } ${props.color ? props.color : "text-interface-text/80"} `}
     >
       {props.label}
     </div>
--- components/structure/pages/Purchase/Quotation/PurchaseQuotation.tsx
+++ components/structure/pages/Purchase/Quotation/PurchaseQuotation.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   ResizableHandle,
   ResizablePanel,
@@ -6,17 +6,104 @@ import {
 } from "@/components/ui/resizable";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import Label from "@/components/myui/Label";
+import { product } from "@prisma/client";
+import { serverActions } from "@/serverActions/serverActions";
+import ListRow from "../../Product/ListRow";
+import InputBox from "@/components/myui/InputBox";
 
 type Props = {};
 
 const PurchaseQuotation = (props: Props) => {
+  const [selectedProduct, setSelectedProduct] = useState<product | null | any>(
+    null
+  );
+  const [filterFound, setFilterFound] = useState(0);
+  const [filterText, setFilterText] = useState("");
+  const [productList, setProductList] = useState<product[] | any>([]);
+
+  const fetchProducts = async () => {
+    const response = await serverActions.product.listAll();
+    setProductList((prev: any) => response.data);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const handleSelectProduct = (product: any) => {
+    setSelectedProduct(product);
+  };
+
+  const handleFilterTextChange = (value: string) => {
+    setFilterText((prev: any) => {
+      countFound(`${value}`);
+      return value;
+    });
+  };
+
+  const countFound = (value: string) => {
+    let count = 0;
+    productList.map((item: product) => {
+      if (item.name.toLowerCase().includes(value.toLowerCase())) {
+        count++;
+      }
+    });
+    setFilterFound(count);
+  };
+
   return (
     <ResizablePanelGroup direction="horizontal">
-      <ResizablePanel defaultSize={40} className="pl-2">
+      <ResizablePanel defaultSize={40} className="w-full">
         <ScrollArea className="h-[88dvh]">
           <div>
-            <Label label="Purchase Quotation" size="text-lg" />
-            <div></div>
+            <Label
+              label="Purchase Quotation"
+              size="text-lg"
+              className="text-center text-2xl my-2"
+              color="text-interface-primary"
+            />
+            <div>
+              <div className="flex flex-col gap-2 pl-2 pr-4 mt-4">
+                <div>
+                  <InputBox
+                    label="Search"
+                    setValue={handleFilterTextChange}
+                    value={filterText}
+                    placeholder="Product"
+                    subLabel={
+                      filterText.length > 0
+                        ? filterFound > 0
+                          ? `Found (${filterFound}) results.`
+                          : "0 results found"
+                        : ""
+                    }
+                  />
+                </div>
+                <div className="">
+                  <ScrollArea className="h-[72dvh]">
+                    {productList.map((product: any, index: number) => {
+                      if (
+                        filterText &&
+                        !product.name
+                          .toLowerCase()
+                          .includes(filterText.toLowerCase())
+                      ) {
+                        return null;
+                      }
+                      return (
+                        <ListRow
+                          key={index}
+                          value={product.name}
+                          product={product}
+                          isSelected={selectedProduct?.name === product.name}
+                          setValue={handleSelectProduct}
+                        />
+                      );
+                    })}
+                  </ScrollArea>
+                </div>
+              </div>
+            </div>
           </div>
         </ScrollArea>
       </ResizablePanel>
