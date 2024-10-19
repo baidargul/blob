9de327c871d29574167e6343331edf8bed2ac19a
Baidar Gul <baidargul@outlook.com>
2024-10-19 17:59:58 +0500

    working on purchase quotation form

--- components/structure/pages/Purchase/Quotation/PurchaseQuotation.tsx
+++ components/structure/pages/Purchase/Quotation/PurchaseQuotation.tsx
@@ -5,6 +5,7 @@ import {
   ResizablePanelGroup,
 } from "@/components/ui/resizable";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import Label from "@/components/myui/Label";
 
 type Props = {};
 
@@ -12,7 +13,12 @@ const PurchaseQuotation = (props: Props) => {
   return (
     <ResizablePanelGroup direction="horizontal">
       <ResizablePanel defaultSize={40} className="pl-2">
-        One
+        <ScrollArea className="h-[88dvh]">
+          <div>
+            <Label label="Purchase Quotation" size="text-lg" />
+            <div></div>
+          </div>
+        </ScrollArea>
       </ResizablePanel>
       <ResizableHandle />
       <ResizablePanel>
