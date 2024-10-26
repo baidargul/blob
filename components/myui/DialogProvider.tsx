a7df140429155277eaab9507fab5b9f367e1e8f7
Baidar Gul <baidargul@outlook.com>
2024-10-26 20:28:12 +0500

    Created DialogProvider

---
+++ components/myui/DialogProvider.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+type Props = {
+  children: React.ReactNode;
+  content: React.ReactNode;
+  title: string;
+};
+
+const DialogProvider = (props: Props) => {
+  return (
+    <Dialog>
+      <DialogTrigger>{props.children}</DialogTrigger>
+      <DialogContent className="p-2">
+        <DialogHeader className="">
+          <DialogTitle>{props.title}</DialogTitle>
+          <DialogDescription className="hidden">
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="">{props.content}</div>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default DialogProvider;
--- components/structure/pages/Purchase/Order/ProductOrderRow.tsx
+++ components/structure/pages/Purchase/Order/ProductOrderRow.tsx
@@ -1,3 +1,4 @@
+import DialogProvider from "@/components/myui/DialogProvider";
 import React from "react";
 
 type Props = {
@@ -7,42 +8,57 @@ type Props = {
 
 const ProductOrderRow = (props: Props) => {
   return (
-    <div className="p-2 bg-white min-w-fit lg:w-full rounded flex gap-1 lg:items-start border border-transparent hover:bg-interface-secondry/30 hover:border-interface-secondry transition-all duration-200 cursor-pointer">
-      <div className="font-bold text-interface-text/30">{props.index + 1}-</div>
-      <div className="flex flex-col items-center lg:items-stretch gap-1 min-w-fit w-full">
-        <div className="font-semibold tracking-tight text-interface-text/80 text-sm flex justify-between items-center">
-          <div className="text-base">
-            {props.item.name}
-            {` [${String(props.item.brand.name).toLocaleUpperCase()}]`}
-          </div>
-          <div className="text-xs tracking-widest hidden lg:block">
-            <div>
-              <div>
-                {props.item.category.name}/{props.item.type.name}
-              </div>
-            </div>
-          </div>
+    <DialogProvider
+      content={<ProductEditor item={{ ...props.item }} />}
+      title="Edit Product"
+    >
+      <div className="p-2 bg-white min-w-fit lg:w-full rounded flex gap-1 lg:items-start border border-transparent hover:bg-interface-secondry/30 hover:border-interface-secondry transition-all duration-200 cursor-pointer">
+        <div className="font-bold text-interface-text/30">
+          {props.index + 1}-
         </div>
-        <div>
-          <div className="text-xs tracking-widest  lg:text-left lg:flex lg:justify-between lg:items-end">
-            <div className="flex gap-1 items-center">
-              <div className="flex gap-1 items-center">
-                <div className="font-semibold">Cost:</div>{" "}
-                {props.item.barcodeRegister[0].cost}
-              </div>
-              <div className="flex gap-1 items-center">
-                <div className="font-semibold">Invoice:</div>{" "}
-                {props.item.barcodeRegister[0].invoice}
+        <div className="flex flex-col items-center lg:items-stretch gap-1 min-w-fit w-full">
+          <div className="font-semibold tracking-tight text-interface-text/80 text-sm flex justify-between items-center">
+            <div className="text-base">
+              {props.item.name}
+              {` [${String(props.item.brand.name).toLocaleUpperCase()}]`}
+            </div>
+            <div className="text-xs tracking-widest hidden lg:block">
+              <div>
+                <div>
+                  {props.item.category.name}/{props.item.type.name}
+                </div>
               </div>
             </div>
-            <div className="border-b text-center lg:text-start">
-              {props.item.barcodeRegister[0].barcode}
+          </div>
+          <div>
+            <div className="text-xs tracking-widest  lg:text-left lg:flex lg:justify-between lg:items-end">
+              <div className="flex gap-1 items-center">
+                <div className="flex gap-1 items-center">
+                  <div className="font-semibold">Cost:</div>{" "}
+                  {props.item.barcodeRegister[0].cost}
+                </div>
+                <div className="flex gap-1 items-center">
+                  <div className="font-semibold">Invoice:</div>{" "}
+                  {props.item.barcodeRegister[0].invoice}
+                </div>
+              </div>
+              <div className="border-b text-center lg:text-start">
+                {props.item.barcodeRegister[0].barcode}
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </DialogProvider>
   );
 };
 
 export default ProductOrderRow;
+
+function ProductEditor({ item }: any) {
+  return (
+    <div>
+      <div>{item.name}</div>
+    </div>
+  );
+}
