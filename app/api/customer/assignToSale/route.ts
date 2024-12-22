3a0c494b6996c9dd47ed582bb38f45ef6b3040a4
Baidar Gul <baidargul@outlook.com>
2024-12-22 20:29:31 +0500

    DevTrack: Files: route.ts, app-paths-manifest.json, pages-manifest.json, server-reference-manifest.js, server-reference-manifest.json, package.json, route.js, vendor.ts, serverActions.ts, accounts.ts, _events.json, index.js, default.d.ts, default.js, edge.d.ts, edge.js, index-browser.js, index.d.ts, wasm.d.ts, wasm.js, build-manifest.json, app-build-manifest.json, react-loadable-manifest.json, interception-route-rewrite-manifest.js, middleware-build-manifest.js, middleware-manifest.json, middleware-react-loadable-manifest.js, next-font-manifest.js, next-font-manifest.json, polyfills.js, _buildManifest.js, _ssgManifest.js, next.js, webpack-runtime.js, customer.ts, main-app.js, webpack.js, 633457081244afec._.hot-update.json. [main]  | Changes in: route.ts; app-paths-manifest.json; pages-manifest.json; server-reference-manifest.js; server-reference-manifest.json; package.json; route.ts; route.js; route.ts; route.js; route.ts; vendor.ts; serverActions.ts; accounts.ts; _events.json; index.js; default.d.ts; default.js; edge.d.ts; edge.js; index-browser.js; index.d.ts; package.json; wasm.d.ts; wasm.js; package.json; build-manifest.json; app-build-manifest.json; react-loadable-manifest.json; interception-route-rewrite-manifest.js; middleware-build-manifest.js; middleware-manifest.json; middleware-react-loadable-manifest.js; next-font-manifest.js; next-font-manifest.json; polyfills.js; _buildManifest.js; _ssgManifest.js; next.js; webpack-runtime.js; customer.ts; route.ts; main-app.js; webpack.js; 633457081244afec._.hot-update.json; route.js; route.ts; route.ts

---
+++ app/api/customer/assignToSale/route.ts
@@ -0,0 +1,70 @@
+import prisma from "@/lib/prisma";
+import { NextRequest } from "next/server";
+
+export async function POST(req: NextRequest) {
+  const response = {
+    status: 500,
+    message: "Internal Server Error",
+    data: null as any,
+  };
+
+  try {
+    const data = await req.json();
+
+    if (!data.customer.id) {
+      response.status = 400;
+      response.message = "Customer ID is required";
+      return new Response(JSON.stringify(response));
+    }
+
+    if (!data.saleId) {
+      response.status = 400;
+      response.message = "Sale ID is required";
+      return new Response(JSON.stringify(response));
+    }
+
+    const sale = await prisma.sale.findUnique({
+      where: {
+        id: data.saleId,
+      },
+    });
+
+    if (!sale) {
+      response.status = 400;
+      response.message = "Sale not found";
+      return new Response(JSON.stringify(response));
+    }
+
+    const customer = await prisma.customer.findUnique({
+      where: {
+        id: data.customer.id,
+      },
+    });
+
+    if (!customer) {
+      response.status = 400;
+      response.message = "customer not found";
+      return new Response(JSON.stringify(response));
+    }
+
+    const updatedsale = await prisma.sale.update({
+      where: {
+        id: data.saleId,
+      },
+      data: {
+        accountId: data.customer.accountId,
+      },
+    });
+
+    response.status = 200;
+    response.message = "customer assigned to sale successfully";
+    response.data = updatedsale;
+    return new Response(JSON.stringify(response));
+  } catch (error: any) {
+    console.log("[SERVER ERROR]: " + error.message);
+    response.status = 500;
+    response.message = error.message;
+    response.data = null;
+    return new Response(JSON.stringify(response));
+  }
+}
