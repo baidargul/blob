3f0e47a03241dba84d6b8fd26cdbbbf123ff29b2
Baidar Gul <baidargul@outlook.com>
2025-01-21 11:51:23 +0500

    added header to ledger window

--- components/structure/pages/Accounts/_components/LedgerWindow.tsx
+++ components/structure/pages/Accounts/_components/LedgerWindow.tsx
@@ -23,17 +23,26 @@ const LedgerWindow = (props: Props) => {
   }, [props.selectedAccount]);
 
   return (
-    <div className="bg-white h-[400px] max-h-[400px] overflow-y-auto w-full rounded drop-shadow-sm">
-      {transactions.map((transaction: any, index: number) => {
-        return (
-          <LedgerRow
-            key={index}
-            transaction={transaction}
-            index={index}
-            transactions={transactions}
-          />
-        );
-      })}
+    <div>
+      <div className="grid grid-cols-5 place-items-start bg-black text-white p-2 rounded-t">
+        <div>Date</div>
+        <div>Type</div>
+        <div>Description</div>
+        <div className="ml-auto pr-3">Amount</div>
+        <div className="ml-auto pr-3">Balance</div>
+      </div>
+      <div className="bg-white h-[400px] max-h-[400px] overflow-y-auto w-full rounded drop-shadow-sm">
+        {transactions.map((transaction: any, index: number) => {
+          return (
+            <LedgerRow
+              key={index}
+              transaction={transaction}
+              index={index}
+              transactions={transactions}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
