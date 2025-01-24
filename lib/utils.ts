7df7f786b4c89e0f0b9229e8bb32e32c68217bd9
Baidar Gul <baidargul@outlook.com>
2025-01-24 08:48:51 +0500

    added vscode intellisense to functions

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,17 +5,35 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function formatCurrency(value: number, currency: "Rs" | "$"): string {
+/**
+ *
+ * @param {number} value - 123456
+ * @param {string} currency - The currency type to format the value with
+ * @requires value - The number to format
+ * @returns {string} - The formatted currency string
+ * @example formatCurrency(123456) => "Rs 123,456"
+ * @description Formats the number with commas and adds the currency symbol
+ */
+
+export function formatCurrency(value: number, currency?: "Rs" | "$"): string {
   // Convert the value to a string and format it with commas
   const formattedValue = value.toLocaleString(); // Automatically formats number with commas
 
   // Return the formatted currency string based on the currency type
-  if (currency === "Rs") {
-    return `Rs ${formattedValue}`;
-  } else {
+  if (currency === "$") {
     return `$ ${formattedValue}`;
+  } else {
+    return `Rs ${formattedValue}`;
   }
 }
+
+/**
+ * @requires value - The text to format
+ * @param {string} value - The text to format
+ * @returns {string} - First letter capitalized text
+ * @example formalizeText("hello") => "Hello"
+ * @description Capitalizes the first letter of the text to maintain front-end consistency
+ */
 
 export function formalizeText(value: string) {
   return value.charAt(0).toUpperCase() + value.slice(1);
