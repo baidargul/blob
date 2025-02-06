8236246ed969562e13f057267aebafc8aca79cea
Baidar Gul <baidargul@outlook.com>
2025-02-06 13:23:46 +0500

    recent profits are now reflected on the side bar

--- components/Sidebar/content/RecentProfits.tsx
+++ components/Sidebar/content/RecentProfits.tsx
@@ -1,23 +1,52 @@
 "use client";
+import { formatCurrency } from "@/lib/utils";
 import { serverActions } from "@/serverActions/serverActions";
-import React from "react";
+import React, { useEffect } from "react";
+import { toast } from "sonner";
 
 type Props = {};
 
 const RecentProfits = (props: Props) => {
+  const [today, setToday] = React.useState<{
+    cost: number;
+    profit: number;
+    sales: number;
+  } | null>(null);
+  const [yesterday, setYesterday] = React.useState<{
+    cost: number;
+    profit: number;
+    sales: number;
+  } | null>(null);
+
+  const [dayBeforeYesterday, setDayBeforeYesterday] = React.useState<{
+    cost: number;
+    profit: number;
+    sales: number;
+  } | null>(null);
+
   const fetchReport = async () => {
     const response = await serverActions.Reports.RecentProfits.getSales();
-    console.log(response);
+    if (response.status === 200) {
+      setToday(response.data.today);
+      setYesterday(response.data.yesterday);
+      setDayBeforeYesterday(response.data.dayBeforeYesterday);
+    } else if (response.status === 400) {
+      toast.warning(response.message);
+    } else {
+      toast.error(response.message);
+    }
   };
 
-  fetchReport();
+  useEffect(() => {
+    fetchReport();
+  }, []);
 
   return (
     <div className="">
       <div className="flex flex-col gap-2">
-        <PanelToday />
-        <PanelYesterday />
-        <PanelDayBeforeYesterday />
+        <PanelToday today={today} />
+        <PanelYesterday yesterday={yesterday} />
+        <PanelDayBeforeYesterday dayBeforeYesterday={dayBeforeYesterday} />
       </div>
     </div>
   );
@@ -25,7 +54,11 @@ const RecentProfits = (props: Props) => {
 
 export default RecentProfits;
 
-const PanelToday = (props: Props) => {
+const PanelToday = ({
+  today,
+}: {
+  today: { cost: number; profit: number; sales: number } | null;
+}) => {
   return (
     <div className="p-4 rounded bg-interface-hover/40">
       <div className="font-semibold text-lg">Today</div>
@@ -33,26 +66,34 @@ const PanelToday = (props: Props) => {
         <div>
           <div className="flex items-center gap-1 text-sm">
             <div className="font-semibold">Sales:</div>
-            <div>Rs 1500</div>
+            <div>{formatCurrency(today?.sales || 0)}</div>
           </div>
           <div className="flex items-center gap-1 text-sm">
             <div className="font-semibold">Worth:</div>
-            <div>Rs 600</div>
+            <div>{formatCurrency(today?.cost || 0)}</div>
           </div>
           <div className="flex items-center gap-1 text-sm">
             <div className="font-semibold">Difference:</div>
-            <div>Rs 900</div>
+            <div>{formatCurrency(today?.profit || 0)}</div>
           </div>
         </div>
         <div className="text-4xl ml-auto my-auto text-interface-primary font-bold">
-          Rs 900
+          {formatCurrency(today?.profit || 0)}
         </div>
       </div>
     </div>
   );
 };
 
-const PanelYesterday = (props: Props) => {
+const PanelYesterday = ({
+  yesterday,
+}: {
+  yesterday: {
+    cost: number;
+    profit: number;
+    sales: number;
+  } | null;
+}) => {
   return (
     <div className="p-4 rounded bg-interface-hover/40">
       <div className="font-semibold text-lg">Yesterday</div>
@@ -60,26 +101,34 @@ const PanelYesterday = (props: Props) => {
         <div>
           <div className="flex items-center gap-1 text-sm">
             <div className="font-semibold">Sales:</div>
-            <div>Rs 1500</div>
+            <div>{formatCurrency(yesterday?.sales || 0)}</div>
           </div>
           <div className="flex items-center gap-1 text-sm">
             <div className="font-semibold">Worth:</div>
-            <div>Rs 600</div>
+            <div>{formatCurrency(yesterday?.cost || 0)}</div>
           </div>
           <div className="flex items-center gap-1 text-sm">
             <div className="font-semibold">Difference:</div>
-            <div>Rs 900</div>
+            <div>{formatCurrency(yesterday?.profit || 0)}</div>
           </div>
         </div>
         <div className="text-4xl ml-auto my-auto text-interface-primary font-bold">
-          Rs 900
+          {formatCurrency(yesterday?.profit || 0)}
         </div>
       </div>
     </div>
   );
 };
 
-const PanelDayBeforeYesterday = (props: Props) => {
+const PanelDayBeforeYesterday = ({
+  dayBeforeYesterday,
+}: {
+  dayBeforeYesterday: {
+    cost: number;
+    profit: number;
+    sales: number;
+  } | null;
+}) => {
   return (
     <div className="p-4 rounded bg-interface-hover/40">
       <div className="font-semibold text-lg">Day before yesterday</div>
@@ -87,19 +136,19 @@ const PanelDayBeforeYesterday = (props: Props) => {
         <div>
           <div className="flex items-center gap-1 text-sm">
             <div className="font-semibold">Sales:</div>
-            <div>Rs 1500</div>
+            <div>{formatCurrency(dayBeforeYesterday?.sales || 0)}</div>
           </div>
           <div className="flex items-center gap-1 text-sm">
             <div className="font-semibold">Worth:</div>
-            <div>Rs 600</div>
+            <div>{formatCurrency(dayBeforeYesterday?.cost || 0)}</div>
           </div>
           <div className="flex items-center gap-1 text-sm">
             <div className="font-semibold">Difference:</div>
-            <div>Rs 900</div>
+            <div>{formatCurrency(dayBeforeYesterday?.profit || 0)}</div>
           </div>
         </div>
         <div className="text-4xl ml-auto my-auto text-interface-primary font-bold">
-          Rs 900
+          {formatCurrency(dayBeforeYesterday?.profit || 0)}
         </div>
       </div>
     </div>
